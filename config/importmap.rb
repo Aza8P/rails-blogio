89b8bca39b4ca6bae455a4c8e7861d59ed9d0273
Aza8P <a3jargal@icloud.com>
2023-05-25 21:32:56 +0800

    added bootstrap datepicker to new article page

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin_all_from "app/javascript/components", under: "components"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "vue", to: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.min.js'
 pin "vue-turbolinks", to: "https://ga.jspm.io/npm:vue-turbolinks@2.2.2/index.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
-pin "boostrap-datepicker", to: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"
+pin "bootstrap-datepicker", to: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"
