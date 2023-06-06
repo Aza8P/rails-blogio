764819f1186904825f97146d9063b96081a7cf02
Aza8P <a3jargal@icloud.com>
2023-06-06 17:54:05 +0800

    fixed the small bug in search vuejs

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,43 +1,46 @@
 // search.js
 import Vue from 'vue';
-// import TurbolinksAdapter from "vue-turbolinks";
-// Vue.use(TurbolinksAdapter);
+import TurbolinksAdapter from "vue-turbolinks";
+Vue.use(TurbolinksAdapter);
 
-const app = new Vue ({
-  el: "#app",
-  data: {
-    query: "",
-    articles: [],
-    currentUser: {
-      id: parseInt(document.body.dataset.userId)
-    }
-  },
-  mounted() {
-    console.log("hello from search.js")
-    const searchForm = document.getElementById("search-form");
-    if (searchForm) {
-      searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        this.query = document.getElementById("query").value;
-        this.searchArticles();
-      }.bind(this));
-    }
-    console.log("currentUser", this.currentUser)
-  }, 
-  methods:{
-    searchArticles() {
-      console.log("hello from searchArticles")
-      console.log(this.query)
-      fetch(`/articles.json?query=${this.query}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log("response is --->", data)
-        this.articles = data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+document.addEventListener('turbo:load', () => {
+  const app = new Vue ({
+    el: "#app",
+    data: {
+      query: "",
+      articles: [],
+      currentUser: {
+        id: parseInt(document.body.dataset.userId)
+      }
     },
-  }
-});
+    mounted() {
+      console.log("hello from search.js")
+      const searchForm = document.getElementById("search-form");
+      if (searchForm) {
+        searchForm.addEventListener('submit', function(event) {
+          event.preventDefault();
+          this.query = document.getElementById("query").value;
+          this.searchArticles();
+        }.bind(this));
+      }
+      console.log("currentUser", this.currentUser)
+    }, 
+    methods:{
+      searchArticles() {
+        console.log("hello from searchArticles")
+        console.log("query is -->", this.query)
+        fetch(`/articles.json?query=${this.query}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log("response is --->", data)
+          this.articles = data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      },
+    }, 
+  });
+})
+
 
